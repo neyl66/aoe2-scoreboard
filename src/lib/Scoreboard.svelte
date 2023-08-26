87ba69b628c055295035be86c895e27ba6399dd1
Neyl66 <neyl.mage@seznam.cz>
2023-08-26 07:26:14 +0200

    🎨 Remove wrong new lines

--- src/lib/Scoreboard.svelte
+++ src/lib/Scoreboard.svelte
@@ -67,7 +67,7 @@
     async function set_data() {
         if (is_loading) return;
         is_loading = true;
-        
+
         // Deduct hours from current time.
         change_hours();
 
@@ -83,7 +83,7 @@
             // Set enemy matches
             matches["enemy"] = await get_matches("enemy", settings.profile_id_enemy).then((matches) => matches.filter((match) => {
                 const {players, finished} = match;
-                
+
                 // Skip games based on number of players.
                 if (match.num_players !== settings.num_players) return false;
 
@@ -95,13 +95,13 @@
 
                 return true;
             }));
-            
+
             // Set player winrate.
             if (settings.profile_id_enemy && matches.enemy.length > 0) {
                 settings.current_match_id = matches["player"][0]?.match_id;
                 set_winrate();
             }
-            
+
         }
 
         is_loading = false;
@@ -269,11 +269,11 @@
     {#if (winrate.number_of_games > 0) }
         <div class="winrate-container">
             <span class="winrate-percentage">{winrate.percentage}%</span>
-            
+
             <svg class="winrate-trophy" xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 24 24">
                 <path fill="#fff" d="M5 0c0 9.803 5.105 12.053 5.604 16h2.805c.497-3.947 5.591-6.197 5.591-16h-14zm7.006 14.62c-.408-.998-.969-1.959-1.548-2.953-1.422-2.438-3.011-5.162-3.379-9.667h9.842c-.368 4.506-1.953 7.23-3.372 9.669-.577.993-1.136 1.954-1.543 2.951zm-.006-3.073c-1.125-2.563-1.849-5.599-1.857-8.547h-1.383c.374 3.118 1.857 7.023 3.24 8.547zm12-9.547c-.372 4.105-2.808 8.091-6.873 9.438.297-.552.596-1.145.882-1.783 2.915-1.521 4.037-4.25 4.464-6.251h-2.688c.059-.45.103-.922.139-1.405h4.076zm-24 0c.372 4.105 2.808 8.091 6.873 9.438-.297-.552-.596-1.145-.882-1.783-2.915-1.521-4.037-4.25-4.464-6.251h2.688c-.058-.449-.102-.922-.138-1.404h-4.077zm13.438 15h-2.866c-.202 1.187-1.63 2.619-3.571 2.619v4.381h10v-4.381c-1.999 0-3.371-1.432-3.563-2.619zm2.562 6h-8v-2h8v2z"></path>
             </svg>
-            
+
             <span class="number-of-players">({winrate.number_of_games})</span>
         </div>
     {/if}
